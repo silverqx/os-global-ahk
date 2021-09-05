42e32e3339c57615a75490f50888971c67acfe72
silverqx <silver.zachara@gmail.com>
2021-09-05 14:30:30 +0200

    updated recompile.ps1 to avoid new warning

--- recompile.ps1
+++ recompile.ps1
@@ -1,6 +1,6 @@
 Stop-ScheduledTask -TaskPath \Crystal\ -TaskName AhkOsGlobal
 
-Start-Process -FilePath Ahk2Exe.exe -ArgumentList "/in os-global.ahk /icon os-global.ico /bin os-global.bin" -Wait -NoNewWindow
+Start-Process -FilePath 'C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe' -ArgumentList "/in os-global.ahk /icon os-global.ico /bin os-global.bin" -Wait -NoNewWindow -WorkingDirectory E:\autohotkey\os-global
 
 Start-ScheduledTask -TaskPath \Crystal\ -TaskName AhkOsGlobal
 
